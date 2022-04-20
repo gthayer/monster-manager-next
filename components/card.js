4234e7a1b3db56bf8aec5c3e08ec756d1ad058cc
Gary Thayer <gary.thayer@10up.com>
2022-04-19 20:40:44 -0400

    slugify the monster name for better link routing

--- components/card.js
+++ components/card.js
@@ -1,11 +1,12 @@
 import Link from 'next/link';
+import { slugifyName } from '../lib/monsters.js';
 
 export default function card({monster}) {
 	return (
 		<div>
 				
 			<h2 className="font-bold text-2xl text-blue-500">
-				<Link href={`/monsters/${monster.name}`}>
+				<Link href={`/monsters/${slugifyName(monster.name)}`}>
 					<a>{monster.name}</a>
 				</Link>
 			</h2>
